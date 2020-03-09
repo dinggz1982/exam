7a7846a6060b768e7ad0662d37830edde8e9441f
dingguozhu <dgz888@163.com>
2020-03-09 22:01:10 +0800

    edit readme and pom.xml

--- src/test/java/gzhu/edu/cn/SearchTest.java
+++ src/test/java/gzhu/edu/cn/SearchTest.java
@@ -2,7 +2,6 @@ package gzhu.edu.cn;
 
 import java.util.List;
 
-import org.apache.jena.base.Sys;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
